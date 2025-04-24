e8958a34825f4e1b402b173af81e090ae2d78c26
Dewi Zheng <dewi.zheng@edu-kw1c.nl>
2025-04-24 22:48:36 +0200

    Importeren autoloader Menu_Database.php en MenuItem.php

--- app/database/Menu_Database.php
+++ app/database/Menu_Database.php
@@ -1,1 +1,13 @@
 <?php
+
+// Volledig pad naar autoload bestand opbouwen
+$autoloadPath = $_SERVER['DOCUMENT_ROOT'] . '/../restaurantkaart/vendor/autoload.php';
+
+// Controleren of het bestand bestaat voordat we het inladen
+if (file_exists($autoloadPath)) {
+    require $autoloadPath;
+    // ✅ Bestand is succesvol ingeladen
+    echo "Het bestand is gevonden";
+} else {
+    echo "❌ Bestand niet gevonden: $autoloadPath";
+}
