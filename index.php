9414897a3c8e308906062aa29e1a29ed6f9192b5
Dewi Zheng <dewi.zheng@edu-kw1c.nl>
2025-04-24 21:24:01 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+// Dan weet ik dat de autoload.php werkt
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    echo "Autoloader is aanwezig! ✅";
+} else {
+    echo "Autoloader niet gevonden ❌";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant kaart Opdracht</title>
+</head>
+
+<body>
+</body>
+
+</html>
